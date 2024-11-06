20a8de1dead33d796e726cd09516fdddd042aa79
Vicent Soria Durá <vicentgodella@gmail.com>
2024-11-06 15:48:58 +0100

    Client side redirect

--- next.config.ts
+++ next.config.ts
@@ -2,15 +2,15 @@ import type { NextConfig } from "next";
 import type {Redirect} from "next/dist/lib/load-custom-routes";
 
 const nextConfig: NextConfig = {
-    redirects: async (): Promise<Redirect[]> => {
-        return [
-            {
-                source: '/:path*',
-                destination: 'https://www.instagram.com/latribudecarla',
-                permanent: false,
-            },
-        ];
-    }
+    // redirects: async (): Promise<Redirect[]> => {
+    //     return [
+    //         {
+    //             source: '/:path*',
+    //             destination: 'https://www.instagram.com/latribudecarla',
+    //             permanent: false,
+    //         },
+    //     ];
+    // }
 };
 
 export default nextConfig;
